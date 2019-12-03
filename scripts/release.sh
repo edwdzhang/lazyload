5b6ff0010a0650069c9b9174f00fddcac62caec1
zhangchengxin <zhangchengxin@meicai.cn>
2019-12-03 16:48:12 +0800

    add release.sh

---
+++ scripts/release.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+git checkout master
+
+VERSION=`npx select-version-cli`
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # build
+  VERSION=$VERSION npm run build
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push origin master
+  git push origin refs/tags/v$VERSION
+
+  npm config set registry=https://registry.npmjs.org/
+  if [[ $VERSION =~ "beta" ]]
+  then
+    npm publish --access public --tag beta
+  else
+    npm publish --access public
+  fi
+
+  echo "Completed"
+fi
