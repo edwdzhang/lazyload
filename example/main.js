4bd94dc87b99059a631bdb848a3d12e98a6aaee1
kimozhang <1193055507@qq.com>
2021-08-30 19:02:45 +0800

    refactor: simplify and remove unused code

--- example/main.js
+++ example/main.js
@@ -1,13 +1,13 @@
-import ll from '../src/index'
+const ll = window.Lazyload
 
-const $ = (selector: string) => {
+const $ = (selector) => {
   return document.querySelectorAll(selector)
 }
 const $init = $('.btn-init')[0]
 const $clean = $('.btn-clean')[0]
 const $show = $('.btn-show')[0]
 
-let unlisten: any
+let unlisten
 
 // initialize listen
 $init.addEventListener(
@@ -21,18 +21,15 @@ $init.addEventListener(
 )
 
 // clean listen
-$clean.addEventListener(
-  'click',
-  () => unlisten && unlisten(),
-  false
-)
+$clean.addEventListener('click', () => unlisten && unlisten(), false)
 
 // show hidden images
 $show.addEventListener(
   'click',
   () => {
-    Array.from($('.lazy-hide'))
-      .forEach(el => el.classList.remove('lazy-hide'))
+    Array.from($('.lazy-hide')).forEach((el) =>
+      el.classList.remove('lazy-hide')
+    )
   },
   false
 )
